c3e3b75008d0cd6127e48329a819dc6ce8a96c8d
Juan Villa <jujotovil@gmail.com>
2024-10-08 23:09:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './platzi.webp';
 import './App.css';
+import React from 'react';
+import { TodoSearch } from './components/TodoSearch';
+import { TodoCounter } from './components/TodoCounter';
+import { TodoList } from './components/TodoList';
+import { TodoItem } from './components/TodoItem';
+import { CreateTodoButton } from './components/CreateTodoButton';
+
+const defaultTodos = [
+  {
+    text: 'Cortar Cebolla',
+    completed: false,
+  },
+
+  {
+    text: 'Completar curso',
+    completed: false,
+  },
+
+  {
+    text: 'Llorar',
+    completed: false,
+  },
+
+  {
+    text: 'Llorar',
+    completed: false,
+  }
+  
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+      <TodoCounter completed={16} total={25}/>
+
+      <TodoSearch/>
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem/>
+        ))}
+      </TodoList>
+      
+
+      <CreateTodoButton/>
+      
+      
+    </React.Fragment>
   );
 }
 
+
 export default App;
+
